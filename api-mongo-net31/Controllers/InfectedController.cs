d83ace8e338132932b83d184b1a8aacd9b4f72f8
Paiva <danpaiva@live.com>
2022-03-12 19:01:46 -0300

    add model and controller
    Former-commit-id: 5dcd5ab22639257fb24ee9549b985a7161724b11

---
+++ api-mongo-net31/Controllers/InfectedController.cs
@@ -0,0 +1,42 @@
+﻿using Api.Data.Collections;
+using api_mongo_net31.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace api_mongo_net31.Controllers
+{
+    public class InfectedController
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class InfectadoController : ControllerBase
+        {
+            Data.MongoDB _mongoDB;
+            IMongoCollection<Infected> _infectadosCollection;
+
+            public InfectadoController(Data.MongoDB mongoDB)
+            {
+                _mongoDB = mongoDB;
+                _infectadosCollection = _mongoDB.DB.GetCollection<Infected>(typeof(Infected).Name.ToLower());
+            }
+
+            [HttpPost]
+            public ActionResult SalvarInfectado([FromBody] InfectedDTO dto)
+            {
+                var infectado = new Infected(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+                _infectadosCollection.InsertOne(infectado);
+
+                return StatusCode(201, "Infectado adicionado com sucesso");
+            }
+
+            [HttpGet]
+            public ActionResult ObterInfectados()
+            {
+                var infectados = _infectadosCollection.Find(Builders<Infected>.Filter.Empty).ToList();
+
+                return Ok(infectados);
+            }
+        }
+    }
+}
---
+++ api-mongo-net31/Models/InfectedDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace api_mongo_net31.Models
+{
+    public class InfectedDTO
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
